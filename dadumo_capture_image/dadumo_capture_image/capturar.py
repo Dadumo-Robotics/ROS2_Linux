0b884cc1bdc9c40c778ef19d6b707f27d99fce93
John Github <lavafrita@gmail.com>
2024-05-22 16:10:59 +0200

    TT-H02-T014
    Ahora funciona en simulacion la cámara

--- dadumo_capture_image/dadumo_capture_image/capturar.py
+++ dadumo_capture_image/dadumo_capture_image/capturar.py
@@ -13,32 +13,50 @@ class Ros2OpenCVImageConverter(Node):
         super().__init__('Ros2OpenCVImageConverter')
 
         self.bridge_object = CvBridge()
-        self.image_sub = self.create_subscription(Image,'/camera/image',self.camera_callback,QoSProfile(depth=10, reliability=ReliabilityPolicy.BEST_EFFORT))
+        self.image_sub = self.create_subscription(Image,'/camera/image_raw',self.camera_callback,QoSProfile(depth=10, reliability=ReliabilityPolicy.BEST_EFFORT))
+        # esto es posible que sea necesario para el robot REAL, image_raw es para SIMULACION
+        #self.image_sub = self.create_subscription(Image,'/camera/image',self.camera_callback,QoSProfile(depth=10, reliability=ReliabilityPolicy.BEST_EFFORT))
 
     def camera_callback(self,data):
-
+        #print("Llamando al callback!")
         try:
             # Seleccionamos bgr8 porque es la codificacion de OpenCV por defecto
             cv_image = self.bridge_object.imgmsg_to_cv2(data, desired_encoding="bgr8")
         except CvBridgeError as e:
             print(e)
 
+        # Aqui ya deberiamos ser capaces de trabajar con la imagen, y trabajar en la detección de objetos y la respuesta del robot a ellos
+        self.centrar_camara(cv_image)
         cv2.imshow("Imagen capturada por el robot", cv_image)
-
         cv2.waitKey(1)
 
-def main(args=None):
+    # esta funcion debe ser llamada cuando se ha CONFIRMADO un objeto, no cuando detecta cualquier cosa
+    def centrar_camara(self,imagen_obstaculo):
+        ancho_imagen = imagen_obstaculo.shape[1]
 
+        centro_imagen = ancho_imagen/2
+        # de momento pilla bien el ancho de la imagen...
+        # el objetivo ahora es que calculemos el centro del objeto de interés, y con ese centro, que intente girar al robot para que centro_imagen
+        # y el centro de interés coincidan. Llamaremos al centro de interés "centro_interes"
+        
+
+        
+
+def main(args=None):
+    #print("Print 1 !!!")
     rclpy.init(args=args)
     img_converter_object = Ros2OpenCVImageConverter()
-
+    #print("PRINT 2 !!!")
     try:
         rclpy.spin(img_converter_object)
+        #print(" PRINT 3 !!!!!")
     except KeyboardInterrupt:
         img_converter_object.destroy_node()
         print("Fin del programa!")
 
     cv2.destroyAllWindows()
+
+
 
 
 if __name__ == '__main__':
